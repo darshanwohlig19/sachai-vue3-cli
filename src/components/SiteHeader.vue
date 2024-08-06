ee849dba7a36807178d5661eca9d68ea5d5d7357
darshanwohlig19 <darshan.gada@wohlig.com>
2024-08-06 15:06:00 +0530

    first commit

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="">
+    <div class="flex justify-between mt-10 mb-10">
+      <div class="flex justify-between w-[30%] items-center">
+        <div class="">
+          <img
+            src="https://uploads-ssl.webflow.com/64ae7a0260c324b7e56ab6b5/64b653319dad7b8061b00de2_sachai-logo.webp"
+            alt=""
+            width="100px"
+          />
+        </div>
+        <div class="border border-solid border-black h-[20px]"></div>
+        <div class="flex justify-between gap-4">
+          <div>
+            <img
+              src="https://uploads-ssl.webflow.com/64ae7a0260c324b7e56ab6b5/64ae7a0360c324b7e56ab783_app-store.svg"
+              alt=""
+              width="100px"
+              height="100px"
+            />
+          </div>
+          <div>
+            <img
+              src="https://uploads-ssl.webflow.com/64ae7a0260c324b7e56ab6b5/64ae7a0360c324b7e56ab783_app-store.svg"
+              alt=""
+              width="100px"
+              height="100px"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-between items-center w-[35%]">
+        <div><a href="/">Home</a></div>
+
+        <div class="dropdown mobile">
+          <button class="dropbtn">
+            Category
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content"></div>
+        </div>
+        <div><a href="">Astrology</a></div>
+
+        <div><a href="/Login">Login</a></div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SiteHeader />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import SiteHeader from "@/components/SiteHeader.vue";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+    SiteHeader,
   },
 };
 </script>
