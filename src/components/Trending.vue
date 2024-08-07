8bf1b7909d97faa33bcfc036a5aa738dac91bf02
darshanwohlig19 <darshan.gada@wohlig.com>
2024-08-07 15:33:07 +0530

    changes

--- src/components/Trending.vue
+++ src/components/Trending.vue
@@ -40,72 +40,19 @@
           </div>
           <div
             class="absolute bottom-0 left-0 w-full p-2 bg-gradient-to-t from-black via-black/60 to-transparent text-white"
-          ></div>
+          >
+            <div class="text-lg">HELLO GUYS WELCOME BACK</div>
+            <div class="text-lg">HELLO GUYS WELCOME BACK</div>
+          </div>
         </div>
       </template>
     </Carousel>
-
-    <div class="flex justify-between items-center">
-      <div class="md:hidden flex items-end justify-end">
-        <button @click="toggleMenu" class="hamburger-btn">&#9776;</button>
-      </div>
-    </div>
-
-    <div :class="['mobile-menu', { show: isMenuOpen }]">
-      <div class="mb-5"><a href="/">Home</a></div>
-      <div class="dropdown mb-5">
-        <button class="dropbtn" @click="toggleDropdown">
-          Category
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content" v-if="isDropdownOpen">
-          <a
-            v-for="heading in categories"
-            :key="heading._id"
-            :href="`/categories/${heading._id}?category=${heading.name}`"
-            class="nav-top"
-          >
-            <div class="text-lg">{{ heading.name }}</div>
-          </a>
-        </div>
-      </div>
-      <div class="mb-5"><a href="">Astrology</a></div>
-      <div class="mb-5"><a href="/Login">Login</a></div>
-    </div>
   </div>
-
-  <div
-    class="text-[45px] font-bold mr-[10px] ml-[10px] sm:mr-[60px] sm:ml-[60px]"
-  >
-    Categories
-  </div>
-  <Catagory />
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
-import { ProductService } from "@/assets/service/ProductService";
-
-const products = ref([]);
-const responsiveOptions = ref([
-  { breakpoint: "1400px", numVisible: 2, numScroll: 1 },
-  { breakpoint: "1199px", numVisible: 3, numScroll: 1 },
-  { breakpoint: "767px", numVisible: 2, numScroll: 1 },
-  { breakpoint: "575px", numVisible: 1, numScroll: 1 },
-]);
-
-const getSeverity = (status) => {
-  switch (status) {
-    case "INSTOCK":
-      return "success";
-    case "LOWSTOCK":
-      return "warn";
-    case "OUTOFSTOCK":
-      return "danger";
-    default:
-      return null;
-  }
-};
+import { ProductService } from "../../src/assets/service/ProductService";
 
 onMounted(() => {
   ProductService.getProductsSmall().then(
@@ -113,16 +60,44 @@ onMounted(() => {
   );
 });
 
-const isMenuOpen = ref(false);
-const isDropdownOpen = ref(false);
-const categories = ref([]);
+const products = ref([]);
+const responsiveOptions = ref([
+  {
+    breakpoint: "1400px",
+    numVisible: 2,
+    numScroll: 1,
+  },
+  {
+    breakpoint: "1199px",
+    numVisible: 3,
+    numScroll: 1,
+  },
+  {
+    breakpoint: "767px",
+    numVisible: 2,
+    numScroll: 1,
+  },
+  {
+    breakpoint: "575px",
+    numVisible: 1,
+    numScroll: 1,
+  },
+]);
 
-const toggleMenu = () => {
-  isMenuOpen.value = !isMenuOpen.value;
-};
+const getSeverity = (status) => {
+  switch (status) {
+    case "INSTOCK":
+      return "success";
 
-const toggleDropdown = () => {
-  isDropdownOpen.value = !isDropdownOpen.value;
+    case "LOWSTOCK":
+      return "warn";
+
+    case "OUTOFSTOCK":
+      return "danger";
+
+    default:
+      return null;
+  }
 };
 </script>
 
@@ -143,13 +118,5 @@ const toggleDropdown = () => {
 
 .carousel .p-carousel-indicator.p-highlight {
   background-color: #fff; /* Highlighted indicator color */
-}
-
-.mobile-menu {
-  display: none;
-}
-
-.mobile-menu.show {
-  display: block;
 }
 </style>
