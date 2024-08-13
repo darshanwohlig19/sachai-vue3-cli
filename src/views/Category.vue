d897d1f07dd3f1b70900bdb769ec66d81cd6bbf2
darshanwohlig19 <darshan.gada@wohlig.com>
2024-08-13 19:17:59 +0530

    \

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,3 +1,44 @@
 <template>
-  <div>Darshan</div>
+  <div>
+    <h1>Category News</h1>
+    <ul>
+      <li v-for="item in news" :key="item.id">{{ item.headline }}</li>
+    </ul>
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import axios from "axios";
+
+const news = ref([]);
+const news1 = ref([]);
+
+const route = useRoute();
+const categoryId = route.params.slugOrId;
+const fetchNews = async () => {
+  try {
+    const response = await axios.post(
+      "https://dev-api.askus.news/news/getCategoryWiseNewsForWeb/",
+      {
+        categoryId,
+      }
+    );
+    news.value = response.data.slice(0, 3);
+    news1.value = response.data.slice(4, length - 1);
+  } catch (error) {
+    console.error("Error fetching news:", error);
+  }
+};
+
+onMounted(() => {
+  if (categoryId) {
+    fetchNews();
+  }
+});
+</script>
+
+<style>
+/* Add your styles here */
+</style>
