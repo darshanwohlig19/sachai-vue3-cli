f58f6ce1713ebe96318719277b99837f6544fb7e
maru-tirth <tirthmaru14@gmail.com>
2024-08-23 19:25:44 +0530

    footer_updated

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="footer">
     <!-- LOGO -->
-    <div class="flex">
-      <div class="w-[35%]">
+    <div class="flex flex-col md:flex-row">
+      <div class="md:w-[35%] mb-6 md:mb-0">
         <img
           src="https://uploads-ssl.webflow.com/64ae7a0260c324b7e56ab6b5/64b653319dad7b8061b00de2_sachai-logo.webp"
           alt="Sachai Logo"
           width="100px"
         />
-        <span class="text-lato font-bold text-[24px] text-[#1E0627] mt-5"
-          >New Age News App Powered by AI</span
-        >
-        <p class="w-[402px] text-[18px] text-lato mt-2">
+        <span class="text-lato font-bold text-[24px] text-[#1E0627] mt-5 block">
+          New Age News App Powered by AI
+        </span>
+        <p class="text-[18px] text-lato mt-2">
           Get trustworthy news in an easy-to-read format, our smart chatbot
           provides a personalized and interactive news experience for you.
         </p>
       </div>
       <!-- Popular Categories -->
-      <div class="w-[35%]">
-        <span class="text-[#FF0053] text-[13px] text-lato font-bold"
-          >Popular Categories</span
-        >
-        <div class="flex gap-[23px]">
-          <div class>
+      <div class="md:w-[35%] mb-6 md:mb-0">
+        <span class="text-[#FF0053] text-[13px] text-lato font-bold block mb-2">
+          Popular Categories
+        </span>
+        <div class="flex flex-wrap gap-[23px]">
+          <div class="w-full md:w-auto">
             <ul>
               <li
                 class="text-[#52525B] text-[14px] text-lato"
@@ -30,90 +30,96 @@
                 :key="heading._id"
               >
                 <a
+                  style="text-transform: capitalize"
                   :href="`/categories/${heading._id}?category=${heading.name}`"
-                  >{{ heading.name }}</a
                 >
+                  {{ heading.name }}
+                </a>
               </li>
             </ul>
           </div>
-          <div class>
+          <div class="w-full md:w-auto">
             <ul>
               <li
                 class="text-[#52525B] text-[14px] text-lato"
-                v-for="heading in categories.slice(5, 8)"
+                v-for="heading in categories.slice(4, 8)"
                 :key="heading._id"
               >
                 <a
+                  style="text-transform: capitalize"
                   :href="`/categories/${heading._id}?category=${heading.name}`"
-                  >{{ heading.name }}</a
                 >
+                  {{ heading.name }}
+                </a>
               </li>
             </ul>
           </div>
-          <div class>
+          <div class="w-full md:w-auto">
             <ul>
               <li
                 class="text-[#52525B] text-[14px] text-lato"
-                v-for="heading in categories.slice(8, 11)"
+                v-for="heading in categories.slice(8, 12)"
                 :key="heading._id"
               >
                 <a
+                  style="text-transform: capitalize"
                   :href="`/categories/${heading._id}?category=${heading.name}`"
-                  >{{ heading.name }}</a
                 >
+                  {{ heading.name }}
+                </a>
               </li>
             </ul>
           </div>
-          <div class>
+          <div class="w-full md:w-auto">
             <ul>
               <li
                 class="text-[#52525B] text-[14px] text-lato"
-                v-for="heading in categories.slice(11, 16)"
+                v-for="heading in categories.slice(12, 16)"
                 :key="heading._id"
               >
                 <a
+                  style="text-transform: capitalize"
                   :href="`/categories/${heading._id}?category=${heading.name}`"
-                  >{{ heading.name }}</a
                 >
+                  {{ heading.name }}
+                </a>
               </li>
             </ul>
           </div>
         </div>
       </div>
       <!-- Latest News -->
-      <div class="w-[15%]">
-        <span class="text-[#FF0053] text-[13px] text-lato font-bold"
-          >Latest News</span
-        >
+      <div class="md:w-[15%] mb-6 md:mb-0">
+        <span class="text-[#FF0053] text-[13px] text-lato font-bold block mb-2">
+          Latest News
+        </span>
         <div>
-          <div class>
-            <ul>
-              <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
-              <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
-              <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
-              <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
-            </ul>
-          </div>
+          <ul>
+            <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
+            <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
+            <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
+            <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
+          </ul>
         </div>
       </div>
       <!-- HELP -->
-      <div class="w-[15%]">
-        <span class="text-[#FF0053] text-[13px] text-lato font-bold">Help</span>
+      <div class="md:w-[15%] mb-6 md:mb-0">
+        <span class="text-[#FF0053] text-[13px] text-lato font-bold block mb-2">
+          Help
+        </span>
         <div>
-          <div class>
-            <ul>
-              <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
-              <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
-              <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
-              <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
-            </ul>
-          </div>
+          <ul>
+            <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
+            <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
+            <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
+            <li class="text-[#52525B] text-[14px] text-lato">Bharat Bandh</li>
+          </ul>
         </div>
       </div>
     </div>
-    <div class="flex mt-[20px]">
+    <div class="flex flex-col md:flex-row mt-[20px]">
       <!-- DOWNLOAD -->
-      <div class="w-[35%]">
+      <div class="md:w-[35%] mb-6 md:mb-0">
         <div class="flex gap-[23px]">
           <div>
             <img
@@ -140,12 +146,12 @@
         </div>
       </div>
       <!-- Hot on the Web -->
-      <div class="w-[32%]">
-        <span class="text-[#FF0053] text-[13px] text-lato font-bold"
-          >Hot on the Web</span
-        >
-        <div class="flex gap-[23px]">
-          <div class>
+      <div class="md:w-[32%] mb-6 md:mb-0">
+        <span class="text-[#FF0053] text-[13px] text-lato font-bold block mb-2">
+          Hot on the Web
+        </span>
+        <div class="flex flex-wrap gap-[23px]">
+          <div class="w-full md:w-auto">
             <ul>
               <li class="text-[#52525B] text-[14px] text-lato">Aloe Vera</li>
               <li class="text-[#52525B] text-[14px] text-lato">
@@ -155,7 +161,7 @@
               <li class="text-[#52525B] text-[14px] text-lato">Health Tips</li>
             </ul>
           </div>
-          <div class>
+          <div class="w-full md:w-auto">
             <ul>
               <li class="text-[#52525B] text-[14px] text-lato">
                 Optical illusion
@@ -170,12 +176,12 @@
         </div>
       </div>
       <!-- Trending Topics -->
-      <div class="w-[32%]">
-        <span class="text-[#FF0053] text-[13px] text-lato font-bold"
-          >Trending Topics</span
-        >
-        <div class="flex gap-[23px]">
-          <div class>
+      <div class="md:w-[32%] mb-6 md:mb-0">
+        <span class="text-[#FF0053] text-[13px] text-lato font-bold block mb-2">
+          Trending Topics
+        </span>
+        <div class="flex flex-wrap gap-[23px]">
+          <div class="w-full md:w-auto">
             <ul>
               <li class="text-[#52525B] text-[14px] text-lato">Aloe Vera</li>
               <li class="text-[#52525B] text-[14px] text-lato">
@@ -185,7 +191,7 @@
               <li class="text-[#52525B] text-[14px] text-lato">Health Tips</li>
             </ul>
           </div>
-          <div class>
+          <div class="w-full md:w-auto">
             <ul>
               <li class="text-[#52525B] text-[14px] text-lato">
                 Optical illusion
@@ -200,7 +206,7 @@
         </div>
       </div>
     </div>
-    <div class="copyright flex justify-center items-center">
+    <div class="copyright flex justify-center items-center mt-6">
       <span class="text-[#52525B] text-[15px]"
         >© 2024, All Rights Reserved</span
       >
@@ -211,6 +217,7 @@
 <script>
 import axios from "axios";
 import { ref, onMounted } from "vue";
+
 export default {
   setup() {
     const categories = ref([]);
@@ -222,7 +229,13 @@ export default {
           "https://dev-api.askus.news/category/getAllCat",
           { langauge: languageId }
         );
-        categories.value = response.data;
+        categories.value = response.data.map((category) => ({
+          ...category,
+          name:
+            category.name.toLowerCase() === "ai"
+              ? category.name.toUpperCase()
+              : category.name,
+        }));
       } catch (error) {
         console.error("Error fetching categories:", error);
       }
@@ -241,11 +254,71 @@ export default {
 
 <style>
 .footer {
-  position: absolute;
+  position: relative;
   width: 100%;
+  padding: 0 20px;
   bottom: 0;
+  left: 0;
 }
-.copyright {
-  margin-top: 40px;
+
+@media (min-width: 768px) {
+  .footer {
+    padding: 0 50px;
+  }
+}
+
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.flex-col {
+  flex-direction: column;
+}
+
+.flex-row {
+  flex-direction: row;
+}
+
+.md\\:w\\-[35\\%] {
+  width: 35%;
+}
+
+.md\\:w\\-[15\\%] {
+  width: 15%;
+}
+
+.md\\:w\\-[32\\%] {
+  width: 32%;
+}
+
+.w-full {
+  width: 100%;
+}
+
+.text-lato {
+  font-family: "Lato", sans-serif;
+}
+
+.mt-6 {
+  margin-top: 24px;
+}
+
+.mb-6 {
+  margin-bottom: 24px;
+}
+
+@media (max-width: 768px) {
+  .text-[14px] {
+    font-size: 12px;
+  }
+
+  .text-[18px] {
+    font-size: 16px;
+  }
+
+  .text-[24px] {
+    font-size: 20px;
+  }
 }
 </style>
