0eec9e1f9c671b1c56e22f64ccbcca8d471f1c3f
darshanwohlig19 <darshan.gada@wohlig.com>
2024-08-26 17:11:22 +0530

    changes

--- src/components/Trending.vue
+++ src/components/Trending.vue
@@ -1,22 +1,19 @@
 <template>
   <div v-if="blogs.length > 0">
-    <div
-      class="text-[45px] font-bold font-lato mt-4 mr-[20px] ml-[20px] sm:mr-[60px] sm:ml-[60px]"
-    >
-      Trending Headlines
-    </div>
     <div class="card">
       <Carousel
         :value="blogs"
         :numVisible="1"
         :numScroll="1"
         :responsiveOptions="responsiveOptions"
-        class="carousel mt-4 custom-carousal"
+        class="carousel w-[56%] mt-4 custom-carousal"
         showIndicators
         circular
       >
         <template #item="slotProps">
-          <div class="relative rounded-[20px] m-2 h-[363px] overflow-hidden">
+          <div
+            class="relative rounded-[20px] m-2 h-[362px] w-[425px] overflow-hidden"
+          >
             <img
               :src="slotProps.data.imgixUrlHighRes"
               :alt="slotProps.data.name"
@@ -28,7 +25,7 @@
               class="absolute top-4 left-4"
             />
             <div
-              class="absolute top-2 right-4 p-2 text-black flex flex-row gap-3 justify-around"
+              class="absolute top-2 right-4 p-2 text-black flex flex-row gap-2 justify-around"
             >
               <div>
                 <i
@@ -45,12 +42,12 @@
             <div
               class="absolute bottom-0 left-0 w-full p-2 bg-gradient-to-t from-black via-black/60 to-transparent text-white"
             >
-              <div class="gap-1 text-white text-xs ml-4 mb-4">
-                <div class="leading-3 sm:w-[100%] md:w-[60%]">
+              <div class="gap-1 text-white text-xs ml-3 mb-2">
+                <div class="leading-3 sm:w-[100%] md:w-[100%]">
                   <a
                     :href="`${SACHAI_NEWS_URL}${slotProps.data._id}`"
                     style="line-height: 1.2"
-                    class="hover:text-current text-[20px]"
+                    class="hover:text-current font-20"
                   >
                     {{ slotProps.data.headline }}
                   </a>
@@ -183,11 +180,18 @@ export default {
   color: green;
   background-color: white;
   position: relative !important;
-  bottom: 25px;
+  bottom: 12px;
   width: 10px;
   border-radius: 10px;
 }
-.p-carousel-content {
+
+.custom-carousal .p-carousel-indicators {
+  position: absolute;
+  right: 10px;
+  bottom: 0px;
+}
+.p-carousel {
   position: relative;
+  width: 440px !important;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,6 @@
       @category-selected="selectCategory"
     /> -->
 
-    <!-- <Trending /> -->
     <!-- <Featured /> -->
     <!-- <Latest /> -->
     <!-- <BrowseByTopics /> -->
@@ -29,7 +28,9 @@
     <Navbarrr />
     <div class="mx-[30px]">
       <FeaturedNews />
+      <Trending />
       <Campaign />
+
       <MoreNews />
     </div>
     <Footer1 />
@@ -40,7 +41,7 @@
 // import HelloWorld from "@/components/HelloWorld.vue";
 // import SiteHeader from "@/components/SiteHeader.vue";
 // import Latest from "@/components/Latest.vue";
-// import Trending from "@/components/Trending.vue";
+import Trending from "@/components/Trending.vue";
 // import BrowseByTopics from "@/components/BrowseByTopics.vue";
 import Footer1 from "@/components/Footer.vue";
 import MoreNews from "@/components/MoreNews.vue";
@@ -65,7 +66,7 @@ export default {
     // Careousel,
     MoreNews, // HelloWorld,
     // SiteHeader,
-
+    Trending,
     Campaign,
 
     // HelloWorld,
