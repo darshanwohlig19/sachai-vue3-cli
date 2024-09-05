f3407c8b916fb9776ee882bb06640eb95dcfd7a1
darshanwohlig19 <darshan.gada@wohlig.com>
2024-09-05 16:39:56 +0530

    changes

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,36 @@
       </div>
     </template>
   </Toast>
-  <!-- <Button @click="showTemplate" label="View" /> -->
+  <Toast position="top-right" group="error" @close="onClose">
+    <template #message="slotProps">
+      <div class="flex flex-row gap-3">
+        <div>
+          <div
+            class="bg-[red] flex justify-center items-center w-[8px] h-[100%] rounded-md"
+          ></div>
+        </div>
+        <div class="flex justify-center items-center">
+          <div
+            class="bg-[red] flex justify-center items-center w-[28px] h-[28px] rounded-full"
+          >
+            <i class="mdi mdi-error text-white"></i>
+          </div>
+        </div>
+
+        <div>
+          <div class="flex flex-col justify-center" style="flex: 1">
+            <div class="font-medium text-lg my-1 text-900">
+              {{ slotProps.message.summary }}
+            </div>
+            <div class="font-medium text-lg mt-1 text-900">
+              {{ slotProps.message.summary2 }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </Toast>
+  <Button @click="showTemplate" label="View" />
 </template>
 <script setup></script>
 
