a3f120c2cb32cec4bcd84209a297eeeb1da4c3bf
wohligpratik <pratik.sawant@wohlig.com>
2024-10-04 12:03:38 +0530

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -87,6 +87,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 router.beforeEach((to, from, next) => {
