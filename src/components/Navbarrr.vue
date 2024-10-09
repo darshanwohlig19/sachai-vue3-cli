b12f15e55d2f33a7d864199c8b3dff0111158bd1
wohligpratik <pratik.sawant@wohlig.com>
2024-10-09 12:44:47 +0530

    changes

--- src/components/Navbarrr.vue
+++ src/components/Navbarrr.vue
@@ -12,7 +12,7 @@
         </a>
       </div>
       <div class="w-[80%] between-992-max-md-1025:w-[90%]">
-        <div class="flex gap-1 sm:gap-3 items-center justify-end">
+        <div class="flex gap-2 sm:gap-3 items-center justify-end">
           <div class="hidden lg:flex head-navs gap-2">
             <!-- <a href="/">Home</a> -->
             <img
